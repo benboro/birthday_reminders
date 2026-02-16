0cc7a4bac5d8d5630b42ced8ee10c1ad020d9412
Ben Borovinsky <bmborovinsky@gmail.com>
2026-02-15 22:03:18 -0800

    fix(02-01): move .task and .onChange inside WindowGroup content (View modifiers, not Scene)

--- BirthdayReminders/App/BirthdayRemindersApp.swift
+++ BirthdayReminders/App/BirthdayRemindersApp.swift
@@ -30,30 +30,15 @@ struct BirthdayRemindersApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if hasCompletedOnboarding {
-                BirthdayListView(syncService: syncService, notificationScheduler: notificationScheduler)
-            } else {
-                OnboardingFlowView(syncService: syncService, notificationScheduler: notificationScheduler)
+            Group {
+                if hasCompletedOnboarding {
+                    BirthdayListView(syncService: syncService, notificationScheduler: notificationScheduler)
+                } else {
+                    OnboardingFlowView(syncService: syncService, notificationScheduler: notificationScheduler)
+                }
             }
-        }
-        .modelContainer(container)
-        .task {
-            syncService.onImportComplete = { [container, notificationScheduler] _ in
-                let context = container.mainContext
-                let descriptor = FetchDescriptor<Person>()
-                guard let people = try? context.fetch(descriptor) else { return }
-                let hour = UserDefaults.standard.object(forKey: "notificationHour") as? Int ?? 9
-                let minute = UserDefaults.standard.object(forKey: "notificationMinute") as? Int ?? 0
-                await notificationScheduler.reschedule(
-                    people: people,
-                    deliveryHour: hour,
-                    deliveryMinute: minute
-                )
-            }
-        }
-        .onChange(of: scenePhase) { _, newPhase in
-            if newPhase == .active {
-                Task {
+            .task {
+                syncService.onImportComplete = { [container, notificationScheduler] _ in
                     let context = container.mainContext
                     let descriptor = FetchDescriptor<Person>()
                     guard let people = try? context.fetch(descriptor) else { return }
@@ -66,6 +51,23 @@ struct BirthdayRemindersApp: App {
                     )
                 }
             }
+            .onChange(of: scenePhase) { _, newPhase in
+                if newPhase == .active {
+                    Task {
+                        let context = container.mainContext
+                        let descriptor = FetchDescriptor<Person>()
+                        guard let people = try? context.fetch(descriptor) else { return }
+                        let hour = UserDefaults.standard.object(forKey: "notificationHour") as? Int ?? 9
+                        let minute = UserDefaults.standard.object(forKey: "notificationMinute") as? Int ?? 0
+                        await notificationScheduler.reschedule(
+                            people: people,
+                            deliveryHour: hour,
+                            deliveryMinute: minute
+                        )
+                    }
+                }
+            }
         }
+        .modelContainer(container)
     }
 }
